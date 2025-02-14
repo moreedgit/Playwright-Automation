226f217191ba36485abe42ced94a30be18c1cfff
moreedgit <moreeds4@gmail.com>
2025-02-14 15:46:26 -0500

    "Forth Commit"

--- tests/Assertions_Demo.spec.js
+++ tests/Assertions_Demo.spec.js
@@ -2,5 +2,5 @@ import { test, expect } from "@playwright/test";
 
 //For complete list of assertion check the Documentation: https://playwright.dev/docs/test-assertions
 test("basic assertins", async ({ page }) => {
-  expect(true, "expected to be true but was false").toBeFalsy();
+  expect(true, "expected to be true but was false").toBeTruthy();
 });
