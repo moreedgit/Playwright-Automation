f071dbacd6d0b988dbe350d9c1b1af63a1925245
moreedgit <moreeds4@gmail.com>
2025-02-14 15:42:39 -0500

    "Third Commit"

---
+++ tests/Assertions_Demo.spec.js
@@ -0,0 +1,6 @@
+import { test, expect } from "@playwright/test";
+
+//For complete list of assertion check the Documentation: https://playwright.dev/docs/test-assertions
+test("basic assertins", async ({ page }) => {
+  expect(true, "expected to be true but was false").toBeFalsy();
+});
